559315a3e74338360d0bcd598ae9d1d8e0426279
Olha Chornei <pradocw8053@gmail.com>
2022-09-14 12:19:25 +0300

    change task-2 (error)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,6 @@
-// Описан в документации
 import flatpickr from 'flatpickr';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
-
-document.querySelector('[data-start]').setAttribute('disabled', '');
-
 const refs = {
   startBtn: document.querySelector('[data-start]'), // Берём кнопку запуска
   timerShow: document.querySelector('.timer'), // Берём блок для показа времени
@@ -15,100 +9,71 @@ const refs = {
   minutes: document.querySelector('[data-minutes').innerHTML,
   seconds: document.querySelector('[data-seconds').innerHTML,
 };
+let endTime = null;
 
-refs.startBtn.addEventListener('click', () => {
-  timer.start();
-});
-
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+//   Принимает число, приводит к строке и добавляет в начало 0, если число меньше 2-х знаков
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    //    Условие если время закончилось то...
-    if (selectedDates[0] < Date.now()) {
-      // Таймер удаляется
-      clearInterval(timer);
-      // Выводит сообщение что время закончилось
-      Notify.success('Please choose a date in the future');
+    if (selectedDates[0] <= Date.now()) {
+      Notify.failure('Please choose a date in the future');
     } else {
       refs.startBtn.removeAttribute('disabled');
       endTime = selectedDates[0];
     }
   },
 };
+flatpickr(document.querySelector('#datetime-picker'), options);
 
 class Timer {
-  constructor({ onTick }) {
-    (this.intervalId = null), (this.isActive = false), (this.onTick = onTick);
-
-    this.init();
-  }
-
-  init() {
-    const time = this.convertMs(0);
-    this.onTick(time);
+  constructor() {
+    (this.intervalId = null),
+      (this.isActive = false),
+      (refs.startBtn.disabled = true);
   }
 
   start() {
     if (this.isActive) {
       return;
     }
-    this.isActive = true;
-
     this.intervalId = setInterval(() => {
       const startTime = Date.now();
       const deltaTime = endTime - startTime;
-      const time = this.convertMs(deltaTime);
-      this.onTick(time);
+      const time = convertMs(deltaTime);
       if (deltaTime <= 1000) {
         clearInterval(this.intervalId);
       }
-      const { days, hours, minutes, seconds } = this.convertMs(deltaTime);
-      console.log(`${days}':${hours}:${minutes}:${seconds}`);
-
-      document.querySelector('[data-days]').innerHTML = days;
-      document.querySelector('[data-hours]').innerHTML = hours;
-      document.querySelector('[data-minutes]').innerHTML = minutes;
-      document.querySelector('[data-seconds]').innerHTML = seconds;
+      this.timerShow(time);
     }, 1000);
   }
-
-  //   Принимает число, приводит к строке и добавляет в начало 0, если число меньше 2-х знаков
-  addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-  }
-
-  convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    const days = this.addLeadingZero(Math.floor(ms / day));
-    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
-    const minutes = this.addLeadingZero(
-      Math.floor(((ms % day) % hour) / minute)
-    );
-    const seconds = this.addLeadingZero(
-      Math.floor((((ms % day) % hour) % minute) / second)
-    );
-
-    return { days, hours, minutes, seconds };
+  timerShow({ days, hours, minutes, seconds }) {
+    refs.days.textContent = pad(days);
+    refs.hours.textContent = pad(hours);
+    refs.minutes.textContent = pad(minutes);
+    refs.seconds.textContent = pad(seconds);
   }
 }
-
-const timer = new Timer({
-  onTick: timerShow,
-});
-
-function timerShow({ days, hours, minutes, seconds }) {
-  (refs.days.textContent = `${days}`),
-    (refs.hours.textContent = `${hours}`),
-    (refs.minutes.textContent = `${minutes}`),
-    (refs.seconds.textContent = `${seconds}`);
-}
-
-// Инициализация библиотеки
-flatpickr(document.querySelector('#datetime-picker'), options);
+const timer = new Timer();
+refs.startBtn.addEventListener('click', () => timer.start());
